cca4b6318506021884cef2f35c61783802b87523
Td () <i.muvunyi@alustudent.com>
2025-08-04 22:47:45 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,18 +7,21 @@ import requests
 
 
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'MyRedditApp/0.0.1'}
+    """Prints the titles of the first 10 hot posts listed in a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Python:top.ten:v1.0 (by /u/fakeuser)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
-        print(None)
+        print("None")
         return
-    
+
     try:
-        posts = response.json()['data']['children']
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
+            print("None")
+            return
         for post in posts:
-            print(post['data']['title'])
+            print(post.get("data", {}).get("title"))
     except Exception:
-        print(None)
+        print("None")
