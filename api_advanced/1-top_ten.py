e72d32d21c36e68b73321f4a58cf01bc93d74cb4
Td () <i.muvunyi@alustudent.com>
2025-08-04 22:39:55 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
-function that queries the 'Reddit API'
+Function that queries the Reddit API
 and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'MyRedditApp/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    
+    try:
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    except Exception:
+        print(None)
